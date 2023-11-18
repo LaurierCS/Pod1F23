c488bbe6230afd2b2b0c0fb1906ca73448688571
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-18 11:07:56 -0500

    Remove the repetitive code from background.js

--- src/background.js
+++ src/background.js
@@ -1,12 +1,1 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => 
-{
-
-  if(tab.url && tab.url.includes("linkedin.com"))
-  {
-    chrome.tabs.sendMessage(tabId,{
-      type: "New",
-
-    })
-  }
-})
-
+// add codes later
