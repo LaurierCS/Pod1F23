dfed1cf0b0e602a46ad2e4e1e4fc29cb77211c7f
Reid-Harrington <Reidmh110@gmail.com>
2023-11-18 12:12:18 -0500

    Update contentScript.js

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,11 +1,20 @@
 
-
 const onNewElementLoaded = () => 
 {
     jobContainers = document.getElementsByClassName("job-card-list__entity-lockup artdeco-entity-lockup artdeco-entity-lockup--size-4 ember-view");
-    
+    //company name, title
     for (let i = 0; i < jobContainers.length; i++) 
     {
+        const cardContainer = jobContainers[i];
+        //Job title
+        const jobTitleContainer = cardContainer.querySelector("a.job-card-container__link");
+        if (jobTitleContainer) 
+        {
+            const textContent = jobTitleContainer.textContent.trim();
+            console.log(textContent);
+        }
+
+
         const exists =  document.getElementsByClassName("checkmark")[i];
         if(!exists)
         {
@@ -16,34 +25,33 @@ const onNewElementLoaded = () =>
             checkmark.height = 70;
 
             jobContainers[i].appendChild(checkmark);
-            console.log(jobContainers[i]);
         }
     }
   };
   
-  const observeDOM = () => 
+const observeDOM = () => 
+{
+  // MutationObserver is a function that looks for changes in the DOM
+  const observer = new MutationObserver((mutations) => 
   {
-    // MutationObserver is a function that looks for changes in the DOM
-    const observer = new MutationObserver((mutations) => 
+    mutations.forEach((mutation) => 
     {
-      mutations.forEach((mutation) => 
+      if (mutation.type === "childList") 
       {
-        if (mutation.type === "childList") 
-        {
-          mutation.addedNodes.forEach((node) => {
-            if (node.nodeType === 1 && node.classList.contains('job-card-list__entity-lockup')) 
-            {
-              onNewElementLoaded();
-            }
-          });
-        }
-      });
+        mutation.addedNodes.forEach((node) => {
+          if (node.nodeType === 1 && node.classList.contains('job-card-list__entity-lockup')) 
+          {
+            onNewElementLoaded();
+          }
+        });
+      }
     });
-  
-    const config = { childList: true, subtree: true };
-    observer.observe(document.body, config);
-  };
-  
-  observeDOM();
+  });
+
+  const config = { childList: true, subtree: true };
+  observer.observe(document.body, config);
+};
+
+observeDOM();
   
   
