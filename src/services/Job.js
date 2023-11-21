a6acb98a6a7190ee66086e9d624089c4c493403c
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-20 19:06:24 -0500

    Add a job function to connect both api and jobservice, to be used with content script

--- src/services/Job.js
+++ src/services/Job.js
@@ -1,4 +1,13 @@
-import { API } from "./API.js";
+import { API } from "./API";
+import { JobService } from "./Job.service";
 
+async function loadData() {
+    const newJobs = await API.fetchItems();
+    return JobService.setJobs(newJobs);
+}
 
+async function getDataFromStorage() {
+    return JobService.getJobs();
+}
 
+export { loadData, getDataFromStorage };
