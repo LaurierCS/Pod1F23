18db877efe2e7fee68b7afe5792b8ea77f415c75
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-20 19:00:57 -0500

    Create an API file to get things from json file

--- src/services/API.js
+++ src/services/API.js
@@ -4,11 +4,6 @@ const API = {
         const result = await fetch(API.url);
         return await result.json();
     },
-    printItems: async function () {
-        const result = await fetch(API.url);
-        const json = await result.json();
-        console.log(json);
-    },
 };
 
 export { API };
