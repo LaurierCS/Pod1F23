936d5bbff22040678f23e85362e24bdae7ba4a61
Reid-Harrington <Reidmh110@gmail.com>
2023-11-29 17:01:06 -0500

    Added indeed support

--- src/contentScript.js
+++ src/contentScript.js
@@ -6,20 +6,18 @@ const CheckMark = () => {
     const checkmark = document.createElement("img");
     checkmark.src = chrome.runtime.getURL("/assets/badge_verified.png");
     checkmark.className = "checkmark";
-    checkmark.width = 70;
-    checkmark.height = 70;
-
+    checkmark.width = 60;
+    checkmark.height = 60;
     return checkmark;
 };
 
 // getting jobs list from storage
 const jobs = await getDataFromStorage();
 
-const onNewElementLoaded = () => {
-    const jobContainers = document.getElementsByClassName(
-        "job-card-list__entity-lockup artdeco-entity-lockup artdeco-entity-lockup--size-4 ember-view"
-    );
-
+const onNewElementLoaded = (isLinkedin) => {
+  if(isLinkedin)
+  {
+    const jobContainers = document.getElementsByClassName("job-card-list__entity-lockup artdeco-entity-lockup artdeco-entity-lockup--size-4 ember-view");
     const getValue = (jobContainer, className) => {
         const item = jobContainer.querySelector(className);
         return item ? item.textContent.trim() : "";
@@ -60,19 +58,65 @@ const onNewElementLoaded = () => {
             cardContainer.setAttribute("addTag", "true");
         }
     }
+  }
+  else if (!isLinkedin)
+  {
+    const jobContainers = document.getElementsByClassName("css-5lfssm eu4oa1w0");
+    const getValue = (jobContainer, className) => {
+      const item = jobContainer.querySelector(className);
+      return item ? item.textContent.trim() : "";
+  };
+
+  for (let i = 0; i < jobContainers.length; i++) {
+      const cardContainer = jobContainers[i];
+      const positionName = getValue(cardContainer, '.jobTitle span');
+
+      const companyName = getValue(
+          cardContainer,
+          '.company_location span'
+      );
+      console.log(companyName);
+
+      const newJob = {
+          positionName,
+          companyName,
+      };
+      const hasAppliedToTheJob = isDuplicate(jobs, newJob);
+
+      if (hasAppliedToTheJob) {
+          const hasTag =
+              cardContainer.hasAttribute("applied") &&
+              cardContainer.hasAttribute("addTag");
+
+          if (!hasTag) {
+              cardContainer.setAttribute("applied", "true");
+              cardContainer.setAttribute("addTag", "false");
+          }
+      }
+
+      if (
+          cardContainer.getAttribute("applied") === "true" &&
+          cardContainer.getAttribute("addTag") === "false"
+      ) {
+          cardContainer.querySelector('.company_location span').appendChild(CheckMark());
+          cardContainer.setAttribute("addTag", "true");
+
+      }
+  }
+  }
+    
 };
 
-const observeDOM = () => {
+const observeDOMLinkedin = () => {
     // MutationObserver is a function that looks for changes in the DOM
     const observer = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
+          console.log("Checking Linkedin...")
             if (mutation.type === "childList") {
                 mutation.addedNodes.forEach((node) => {
-                    if (
-                        node.nodeType === 1 &&
-                        node.classList.contains("job-card-list__entity-lockup")
-                    ) {
-                        onNewElementLoaded();
+                    if (node.nodeType === 1 && node.classList.contains("job-card-list__entity-lockup")) 
+                    {
+                      onNewElementLoaded(true);
                     }
                 });
             }
@@ -82,5 +126,42 @@ const observeDOM = () => {
     const config = { childList: true, subtree: true };
     observer.observe(document.body, config);
 };
+const observeDOMIndeed = () => {
+  // MutationObserver is a function that looks for changes in the DOM
+  const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        console.log("Checking Indeed...")
+          if (mutation.type === "childList") {
+              mutation.addedNodes.forEach((node) => {
+                  if(node.nodeType === 1 && node.classList.contains("e37uo190"))
+                  {
+                    onNewElementLoaded(false);
+                  }
+                  else if(node.nodeType === 1 && node.classList.contains("eu4oa1w0"))
+                  {
+                    onNewElementLoaded(false);
+                  }
+              });
+          }
+      });
+  });
 
-observeDOM();
+  const config = { childList: true, subtree: true };
+  observer.observe(document.body, config);
+};
+
+if (document.location.href.startsWith("https://ca.indeed.com/"))
+{
+  observeDOMIndeed();
+}
+else if (document.location.href.startsWith("https://www.linkedin.com/"))
+{
+  observeDOMLinkedin();
+
+}
+
+
+
+
+
+
